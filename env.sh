0f83d2896ccd895c6b93a9bab8a00c9495acfbfb
Mike Tay <michael.tay@blueacorn.com>
2024-01-14 20:43:13 -0500

    moving fzf functions to custom

--- env.sh
+++ env.sh
@@ -17,37 +17,6 @@ function mkcd() { mkdir -p "$@" && cd "$_"; }
 alias cppcompile='c++ -std=c++11 -stdlib=libc++'
 alias g='git'
 
-# fuzzy history
-# fh - search in your command history and execute selected command
-fh() {
-  eval $( ([ -n "$ZSH_NAME" ] && fc -l 1 || history) | fzf +s --tac | sed 's/ *[0-9]* *//')
-}
-
-# fuzzy dir
-#:fd - cd to selected directory
-fd() {
-  local dir
-  dir=$(find ${1:-.} -path '*/\.*' -prune \
-                  -o -type d -print 2> /dev/null | fzf +m) &&
-  cd "$dir"
-}
-
-# fuzzy ch - browse chrome history
-ch() {
-  local cols sep
-  cols=$(( COLUMNS / 3 ))
-  sep='{::}'
-
-  cp -f ~/Library/Application\ Support/Google/Chrome/Profile\ 1/History /tmp/h
-
-  sqlite3 -separator $sep /tmp/h \
-    "select substr(title, 1, $cols), url
-     from urls order by last_visit_time desc" |
-  awk -F $sep '{printf "%-'$cols's  \x1b[36m%s\x1b[m\n", $1, $2}' |
-  fzf --ansi --multi | sed 's#.*\(https*://\)#\1#' | xargs open
-}
-
-
 # Re source Zsh
 alias zsrc='source ~/.zshrc'
 alias zenv='vim ~/.oh-my-zsh/env.sh'
