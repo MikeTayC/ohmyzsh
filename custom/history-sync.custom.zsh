babfb00c756b9ff6ef66b3cd1c0ea4c068cde282
Mike Tay <michael.tay@blueacorn.com>
2024-02-03 16:00:45 -0500

    Oh my history sync change

--- custom/history-sync.custom.zsh
+++ custom/history-sync.custom.zsh
@@ -5,7 +5,7 @@
 # your zsh_history file location
 ZSH_HISTORY_FILE="${HOME}/.zsh_history"
 # your git project for housing your zsh_history file
-ZSH_HISTORY_PROJ="${HOME}/dots/drunk-zsh-history"
+ZSH_HISTORY_PROJ="${HOME}/.zsh_history_backups/drunk-zsh-history"
 # your encrypted zsh_history file location
 ZSH_HISTORY_FILE_ENC="${ZSH_HISTORY_PROJ}/zsh_history"
 # your default message when pushing to $ZSH_HISTORY_PROJ
