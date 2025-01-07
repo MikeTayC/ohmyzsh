fa6ef23e3f431fd90d371350eda83404d32a660d
Mike Tay <michael.tay@blueacorn.com>
2025-01-07 16:15:26 -0500

    adding iterm-semantic-history function to open files or directories with custom iterm coprocess

--- custom/apple.zsh
+++ custom/apple.zsh
@@ -20,3 +20,29 @@ function rmd () {
   osascript -e 'tell app "finder" to get posix path of (get desktop picture as alias)';
 }
 
+function iterm-semantic-history () { 
+    # Excluded values
+    local excluded=(
+    	.DS_Store
+	.CFUserTextEncoding
+	.zsh_history.backup
+	.zsh_history
+	img
+	jpg
+	dmg
+	zip
+	.localized
+    )
+
+    if (($excluded[(Ie)$1:t] || $excluded[(Ie)$1:t:e])); then
+	return 1
+    fi
+
+    if [[ -d $1 ]]; then
+        cd $1 && ll
+    elif [[ -f $1 ]]; then
+        vim $1
+    else
+        echo "$1 - value is probably not a directory or file"
+    fi
+}
