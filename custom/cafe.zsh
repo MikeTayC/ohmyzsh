d34166665788ce31e3186091577cbf5130b6b420
Mike Tay <michael.tay@blueacorn.com>
2025-02-06 16:52:02 -0500

    mise using PATH instead of shims; cafe.zsh for caffeinate

---
+++ custom/cafe.zsh
@@ -0,0 +1,37 @@
+###
+# Replacement script for caffiene/amphetamine
+# uses builtin shell command: caffeinate
+# And custom terminal countdown timer for UI: https://github.com/antonmedv/countdown
+# requires command 
+# $ brew install countdown
+#
+# usage:
+# $ cafe [n]
+# where [n] is time in hours
+#
+# caffeinate should run in background while countdown runs
+#
+# Notes:
+# https://aweirdimagination.net/2020/06/28/kill-child-jobs-on-script-exit/
+# https://git.aweirdimagination.net/perelman/kill-child-jobs/src/branch/ master/pkill-P-full-example. sh
+# From https://unix.stackexchange.com/a/240736
+###
+function cafe() {
+  sec=$(($1*3600))
+  # background process started in subshell - hard to kill - gave up and used pkill (grep pid killer)
+  (caffeinate -disu -t $sec &)
+
+  if type "countdown" > /dev/null; then
+     countdown "$sec"s
+  fi
+
+  wait
+
+  echo "Cafe finished.. kill process pids"
+  echo "Killing caffeinate processes.."
+  pkill -f -l "caffeinate"
+  
+  echo "Killing countdown processes.."
+  pkill -f -l "countdown"
+}
+
