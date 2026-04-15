b39ec36cbb05b40e04db5693b9d43c09bc7674a1
Mike Tay <michael.tay@blueacorn.com>
2026-04-15 11:41:44 -0400

    add custom terragrunt plugin for autocompeltion/aliases

---
+++ custom/plugins/terragrunt/terragrunt.plugin.zsh
@@ -0,0 +1,26 @@
+# This custom plugin effectively does what `terragrunt --install-autocomplete` does, except adds it here instead of directly in ~/.zshrc
+
+# only init if not already loaded
+(( $+functions[bashcompinit] )) || autoload -U +X bashcompinit && bashcompinit
+
+complete -o nospace -C $HOMEBREW_PREFIX/bin/terragrunt terragrunt
+
+
+
+# Aliases
+alias cterra="cat $ZSH_CUSTOM/plugins/terragrunt/terragrunt.plugin.zsh"
+alias tg='terragrunt'
+
+# Modern “run style” helpers
+alias tgp='terragrunt run -- plan'
+alias tga='terragrunt run -- apply'
+alias tgi='terragrunt run -- init'
+alias tgv='terragrunt run -- validate'
+
+# State inspection helpers
+alias tgs='terragrunt run -- state list'
+alias tgsh='terragrunt run -- state show'
+
+# Debuggers
+alias tgplan='terragrunt run -- plan -out=tfplan'
+alias tgshow='terragrunt run -- show'
